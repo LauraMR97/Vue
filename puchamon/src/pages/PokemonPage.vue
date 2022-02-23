dc2865424602cc6d336cf4f7d1f04f4dff3abfe6
Laura <lauramorenoramos97@gmail.com>
2022-02-23 14:24:32 +0100

    pokemon funcional, faltan test

--- puchamon/src/pages/PokemonPage.vue
+++ puchamon/src/pages/PokemonPage.vue
@@ -1,18 +1,74 @@
 <template>
-  <h1>¿Quien es este Pokemon?</h1>
-</template>
+<div>
+<h1 v-if="!pokemon">Espere por favor...</h1>
+<div v-else>
+  <h1>Cual es este Pokemon?</h1>
 
-<PokemonPicture :pokemonId="17" :pokemonShow:"true"/>
-<PokemonOptions/>
+<pokemon-picture :pokemonId="pokemon.id" :showPokemon="showPokemon"></pokemon-picture>
+<PokemonOptions :pokemons="pokemonArr" @selectedPokemon="checkAnswer"/>
+
+      <template v-if="showAnswer">
+        <h2>Tienes {{puntos}} puntos</h2>
+      <h2 class="fade-in">{{ message }}</h2>
+      <button @click="nuevoJuego">Nuevo Juego</button>
+      </template>
+</div>
+</div>
+</template>
 
 
 <script>
 import PokemonOptions from '../components/PokemonOptions.vue'
 import PokemonPicture from '../components/PokemonPicture.vue'
-import getPokemonOptions from '../helpers/getPokemonOptions.vue'
+import getPokemonOptions from '../helpers/getPokemonOptions'
 
 export default {
-components: {PokemonPicture,PokemonOptions}
+components: {PokemonPicture,PokemonOptions},
+data(){
+  return{
+    pokemonArr: [],
+    pokemon: null,
+    showPokemon: false,
+    showAnswer: false,
+    message: '',
+    puntos: 0
+  }
+},
+methods: {
+  async mixPokemonArray(){
+    this.pokemonArr = await getPokemonOptions()
+    const randInt = Math.floor(Math.random()*4)
+    this.pokemon = this.pokemonArr[randInt]
+  },
+  checkAnswer(selectedPokemon){
+    this.showPokemon=true;
+    this.showAnswer=true;
+
+    if(selectedPokemon=== this.pokemon.id){
+      this.message = `Correcto, es ${this.pokemon.name}`
+      this.puntos++;
+
+      if(this.puntos==10){
+        this.message = `Has ganado, Enhorabuena`
+        this.puntos=0;
+      }
+    }else{
+       this.message = `Oooooooopsssss, era ${this.pokemon.name}`
+       this.puntos=0;
+    }
+  },
+  nuevoJuego(){
+  this. pokemonArr =[],
+   this. pokemon =null,
+   this. showPokemon= false,
+   this. showAnswer= false,
+  this.  message= '',
+  this.mixPokemonArray()
+  }
+},
+mounted(){
+  this.mixPokemonArray()
+}
 }
 </script>
 
